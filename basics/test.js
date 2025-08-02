cc1de2a99a7015239baccc8299d466bf420ae1e2
KUNALRAWAT789 <rawat98766@gmail.com>
2025-08-02 22:28:16 +0530

    Add files via upload

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,27 @@
 console.log("KUNAL RAWAT")
+let newvar = "hello"
+console.log(newvar)
+const x = 1000
+console.table([newvar,x])
+
+
+
+let number = "55"
+console.log(typeof number)
+let actualvalue = Number(number)
+let bool = Number(true)
+console.log(bool)
+console.log(typeof actualvalue)
+// when we try to print the actual value
+console.log(actualvalue)
+//"55" => 55
+// NULL => 0
+// undefined => NaN
+// "55afc" => NaN
+// "KUNAL" => true
+// "" => false
+// 1 => true
+// 0 => false
+console.log(2**5)
+// == (equals to) and >,<,<=,>=(comparisons) there working syntax differ from each other  
+// ===(strict check does not automatically convert data types as it compares teh data types to make sure they are the same)
