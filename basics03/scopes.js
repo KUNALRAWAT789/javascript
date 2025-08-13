50c8d7d90b8ec16f58e702bf771f13ecf627a47a
KUNALRAWAT789 <rawat98766@gmail.com>
2025-08-13 23:51:55 +0530

    Add files via upload

--- basics03/scopes.js
+++ basics03/scopes.js
@@ -1,10 +1,40 @@
 // Global scope in IDE and on webpage is different
-let a = 50 //global scope 
-d = 90 // var datatype by default
-if(true){
-    let a = 10 //completely new variable with scope within the curly braces;
-   var d = 60
+// let a = 50 //global scope 
+// d = 90 // var datatype by default
+// if(true){
+//     let a = 10 //completely new variable with scope within the curly braces;
+//    var d = 60
+// }
+
+// console.log(a)
+// console.log(d)
+
+// function one(){
+//     const username = "Kunal"
+    
+//     function two(){
+//         const website = "Youtube"
+//         console.log(username)
+//     }
+//     // console.log(website) //will give error since it cant be accessed
+//     two()
+// }
+// one() //when this executes only then the inner function executes
+
+// if(true){
+//     const username = "Kunal"
+//     if(true){
+//        const website = " Youtube"
+//        console.log(username + website) 
+//     }
+//     // console.log(website) //will give error since it cant be accessed
+// }
+
+//+++++++++++++++++ Interesting++++++++++++++++++++//
+// Function expresions 
+
+const addtwo = function(num){
+    return num+2
 }
 
-console.log(a)
-console.log(d)
+console.log(addtwo(5))
